bd4876fee3bfa7f76c390ca6d9585b258f129f4c
Algirdas <alzarkaitis22@gmail.com>
2022-03-02 21:58:26 +0200

    Added ability to start private messaging from catalog

--- oop/app/design/inbox/chat.php
+++ oop/app/design/inbox/chat.php
@@ -8,7 +8,6 @@ use Controller\Inbox;
 <div class="container">
 
     <h2>Chat</h2>
-
     <div class="chat">
         <form action="<?= $this->url('inbox/create') ?>" method="POST">
             <textarea name="content" cols="40" rows="5"></textarea>
@@ -18,18 +17,17 @@ use Controller\Inbox;
     </div>
 
     <?php foreach ($this->data['messages'] as $message) { ?>
-        <hr>
-        <div class="list-item">
+        <div class="chat-wrapper">
             <?php if ($message->getUserId() == $_SESSION['user_id']) {
                 $user = new User($message->getRecipient()) ?>
-                <div class="message-from">
+                <div class="my-message">
                     <h3>To: <?= $user->getFullName(); ?></h3>
                     <p><?= $message->getText(); ?></p>
                     <p><?= $message->getCreatedAt(); ?></p>
                 </div>
             <?php } else {
                 $user = new User($message->getUserId()) ?>
-                <div class="my-message">
+                <div class="message">
                     <h3>From: <?= $user->getFullName(); ?></h3>
                     <p><?= $message->getText(); ?></p>
                     <p><?= $message->getCreatedAt(); ?></p>
