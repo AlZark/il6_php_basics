6d7592aff993d2cad72b077d617aaa6cdcffbb27
Algirdas <alzarkaitis22@gmail.com>
2022-03-23 19:30:58 +0200

    pushing whats left

--- oop/app/code/Controller/Export.php
+++ oop/app/code/Controller/Export.php
@@ -2,7 +2,68 @@
 
 namespace Controller;
 
-class Export
-{
+use Helper\Url;
+use Model\Catalog;
+use Core\AbstractController;
+use Model\Manufacturer;
+use Model\Model;
+use Model\Type;
 
+class Export extends AbstractController
+{
+    public function __construct()
+    {
+        parent::__construct();
+        if (!$this->isUserLoggedIn()) {
+            Url::redirect('user/login');
+        }
+    }
+
+    public function execute(): void
+    {
+        $userAds = Catalog::getAllUserAds($_SESSION['user_id']);
+        $csvPath = PROJECT_ROOT_DIR."/var/Export/adExport.csv";
+        $f = fopen($csvPath, 'w', true);
+        if ($f === false) {
+            $_SESSION['Fail'] = "Failed to export ads";
+            Url::redirect('catalog/my');
+        }
+
+        $firstRow = ['title', 'description', 'manufacturer', 'model', 'price', 'year', 'type', 'img', 'vin', 'mileage',
+            'color'
+        ];
+        fputcsv($f, $firstRow);
+
+        foreach ($userAds as $ad) {
+            $manufacturer = new Manufacturer();
+            $manufacturer->load($ad->getManufacturerId());
+            $manufacturerName = $manufacturer->getName();
+
+            $model = new Model();
+            $model->load($ad->getModelId());
+            $modelName = $model->getName();
+
+            $type = new Type();
+            $type->load($ad->getTypeId());
+            $typeName = $type->getName();
+
+            $row = [
+                'title' => $ad->getTitle(),
+                'description' => $ad->getDescription(),
+                'manufacturer' => $manufacturerName,
+                'model' => $modelName,
+                'price' => $ad->getPrice(),
+                'year' => $ad->getYear(),
+                'type' => $typeName,
+                'img' => $ad->getImg(),
+                'vin' => $ad->getVin(),
+                'mileage' => $ad->getMileage(),
+                'color' => $ad->getColor()
+            ];
+            fputcsv($f, $row);
+        }
+        fclose($f);
+        $_SESSION['success'] = "Ads exported successfully";
+        Url::redirect('catalog/my');
+    }
 }
